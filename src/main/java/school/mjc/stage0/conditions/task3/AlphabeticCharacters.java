0270380209a8f147106a197fdba3a6cfda0a6448
JavohirXoldorov <javohirxoldorov7@gmail.com>
2022-07-31 06:48:12 +0500

    testing

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 97 && character <= 122) || (character >= 65 && character <= 90)) {
+            if (character >= 65 && character <= 90) character += 32;
+            if (character == 'a' || character == 'o' || character == 'e' || character == 'u' || character == 'i') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            int days = 28;
+            if (month == 4 || month == 6 || month == 9 || month == 11) {
+                days = 30;
+            } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                days = 31;
+            System.out.println(days);
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
